7d66408470965aa4214452aa4385d25d1b0fdcca
Elyvaldson <elyvaldson@hotmail.com>
2018-04-26 11:06:49 -0300

    Projeto criado

---
+++ src/main/java/com/glpv/sistemaboleto/BoletoglpvApplication.java
@@ -0,0 +1,12 @@
+package com.glpv.sistemaboleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletoglpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoletoglpvApplication.class, args);
+	}
+}
---
+++ src/test/java/com/glpv/sistemaboleto/BoletoglpvApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glpv.sistemaboleto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoletoglpvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
